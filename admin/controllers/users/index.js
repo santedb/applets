fbf8c045850c2d6f2056f149c6dcd4be7b2dab4a
Justin Fyfe <justin@fyfesoftware.ca>
2019-03-02 17:32:56 -0500

    Updated the index controller

--- admin/controllers/users/index.js
+++ admin/controllers/users/index.js
@@ -9,6 +9,6 @@ angular.module('santedb').controller('UserIndexController', ["$scope", "$rootSco
      * @summary Render the lockout status
      */
     $scope.renderLockout = function(user) {
-        return user.lockout > new Date() ? `<i class="fa fa-lock"></i> ${moment(d).format(SanteDB.locale.dateFormats.second)}` : '<i class="fa fa-lock-open"></i>';
+        return user.lockout > new Date() ? `<i class="fa fa-lock"></i> ${moment(user.lockout).format(SanteDB.locale.dateFormats.second)}` : '<i class="fa fa-lock-open"></i>';
     }
 }]);
