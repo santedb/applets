e4c8e69d82404fa2b234e445c9094dab3ad4c315
Justin Fyfe <justin@fyfesoftware.ca>
2019-09-19 13:38:10 -0400

    Added lock and unlock wire up to devices

--- admin/controllers/devices/index.js
+++ admin/controllers/devices/index.js
@@ -4,14 +4,48 @@ angular.module('santedb').controller('DeviceIndexController', ["$scope", "$rootS
      * @summary Delete the specified user
      */
     $scope.delete = function (id, index) {
-        if (confirm(SanteDB.locale.getString("ui.admin.devices.confirmDelete"))) {
+
+        var data = $("#SecurityDeviceTable table").DataTable().row(index).data();
+        if (!data.obsoletionTime && confirm(SanteDB.locale.getString("ui.admin.devices.confirmDelete"))) {
             $("#action_grp_" + index + " a").addClass("disabled");
             $("#action_grp_" + index + " a i.fa-trash").removeClass("fa-trash").addClass("fa-circle-notch fa-spin");
             SanteDB.resources.securityDevice.deleteAsync(id)
                 .then(function (e) {
+                    $("#SecurityDeviceTable").attr("newQuery", true);
                     $("#SecurityDeviceTable table").DataTable().draw();
                 })
                 .catch($rootScope.errorHandler);
+        }
+        else if (data.obsoletionTime && confirm(SanteDB.locale.getString("ui.admin.devices.confirmUnDelete"))) {
+            $("#action_grp_" + index + " a").addClass("disabled");
+            $("#action_grp_" + index + " a i.fa-trash-restore").removeClass("fa-trash-restore").addClass("fa-circle-notch fa-spin");
+
+            // Patch the user
+            var patch = new Patch({
+                change: [
+                    new PatchOperation({
+                        op: PatchOperationType.Remove,
+                        path: 'obsoletionTime',
+                        value: null
+                    }),
+                    new PatchOperation({
+                        op: PatchOperationType.Remove,
+                        path: 'obsoletedBy',
+                        value: null
+                    })
+                ]
+            });
+
+            SanteDB.resources.securityDevice.patchAsync(id, data.securityStamp, patch)
+                .then(function (e) {
+                    $("#SecurityDeviceTable").attr("newQuery", true);
+                    $("#SecurityDeviceTable table").DataTable().draw();
+                })
+                .catch(function (e) {
+                    $("#action_grp_" + index + " a").removeClass("disabled");
+                    $("#action_grp_" + index + " a i.fa-circle-notch").removeClass("fa-circle-notch fa-spin").addClass("fa-trash-restore");
+                    $rootScope.errorHandler(e);
+                });
         }
     }
 
@@ -23,6 +57,7 @@ angular.module('santedb').controller('DeviceIndexController', ["$scope", "$rootS
         if (confirm(SanteDB.locale.getString(data.lockout ? "ui.admin.devices.confirmUnlock" : "ui.admin.devices.confirmLock"))) {
             $("#action_grp_" + index + " a").addClass("disabled");
             $("#action_grp_" + index + " a i.fa-lock").removeClass("fa-lock").addClass("fa-circle-notch fa-spin");
+            $("#action_grp_" + index + " a i.fa-unlock").removeClass("fa-unlock").addClass("fa-circle-notch fa-spin");
             if (data.lockout) {
                 SanteDB.resources.securityDevice.unLockAsync(id)
                     .then(function (e) {
@@ -53,7 +88,7 @@ angular.module('santedb').controller('DeviceIndexController', ["$scope", "$rootS
      * @summary Render updated by
      */
     $scope.renderUpdatedBy = function (device) {
-        if(device.obsoletedBy != null)
+        if (device.obsoletedBy != null)
             return `<provenance provenance-id="'${device.obsoletedBy}'" sessionfn="$parent.sessionFunction" provenance-time="'${device.obsoletedBy}'"></provenance>`;
         else if (device.updatedBy != null)
             return `<provenance provenance-id="'${device.updatedBy}'" sessionfn="$parent.sessionFunction" provenance-time="'${device.updatedTime}'"></provenance>`;
