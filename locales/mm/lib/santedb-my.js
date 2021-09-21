93613dfa0e5437e13732f620de936612e29fb5fb
Justin Fyfe <justin@fyfesoftware.ca>
2021-09-21 11:13:49 -0400

    Added myanmar MPI translations (portion) to test

---
+++ locales/mm/lib/santedb-my.js
@@ -0,0 +1,10 @@
+SanteDB.locale.dateFormats = {
+    year: 'YYYY',
+    month: 'YYYY-MM',
+    day: 'DD MM YYYY',
+    hour: 'DD MM YYYY HH',
+    minute: 'DD MM YYYY HH:mm',
+    second: 'DD MM YYYY HH:mm:ss'
+}
+
+SanteDB.display.setPreferredNameType("Phonetic");
