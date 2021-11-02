25ea80a55bf8779d05f14c5a098003b094e07b9c
Justin Fyfe <justin@fyfesoftware.ca>
2021-11-02 12:15:48 -0400

    Updated getString to accept parameters

--- core/js/santedb.js
+++ core/js/santedb.js
@@ -3633,11 +3633,18 @@ function SanteDBWrapper() {
             * @memberof SanteDBWrapper.LocalizationApi
             * @method getString
             * @param {string} stringId The id of the localization string to get
+            * @param {any} parameters The parameters used to substitute the string value
             * @returns {string} The localized string
             */
-        this.getString = function (stringId) {
+        this.getString = function (stringId, parameters) {
             try {
                 var retVal = __SanteDBAppService.GetString(stringId);
+
+                if(retVal) {
+                    retVal = retVal.replace(/(\{.*\})/ig, function(s) {
+                        return parameters[s];
+                    });
+                }
                 return retVal || stringId;
             }
             catch (e) {
