72e0e2bf51787a0ab53638fe0f1609821b0fa0c9
Justin Fyfe <justin@fyfesoftware.ca>
2022-11-13 11:28:24 -0500

    Updated the data quality panel

--- uicore/controllers/widgets/dataQuality.js
+++ uicore/controllers/widgets/dataQuality.js
@@ -1,6 +1,17 @@
 /// <reference path="../../../core/js/santedb.js"/>
-angular.module('santedb').controller('DataQualityWidgetController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+angular.module('santedb').controller('DataQualityWidgetController', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {
     
+
+    async function loadTypes() {
+        var types = await SanteDB.resources.concept.findAsync({ 'conceptSet' : '9167DDAF-D29A-4448-9F72-887DF3A5BD3E' });
+        $timeout(_ => {
+            $scope.detectedIssueTypes = {};
+            types.resource.forEach(t=> $scope.detectedIssueTypes[t.id] = t);
+        });
+    }
+
+    loadTypes();
+
     $scope.$watch("scopedObject", function(n, o) {
         if(n && n.extension['http://santedb.org/extensions/core/detectedIssue'] && !n.dataQualityIssues)
             try {
