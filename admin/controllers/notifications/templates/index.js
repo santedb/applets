3291645b0aff806114e8846883b88bedb315ac88
Jordan Webber <webberjo@hhsc.ca>
2025-04-28 11:28:51 -0400

    JIMS-713 Added functionality to soft delete a Notification Template in the Notification Template table.

--- admin/controllers/notifications/templates/index.js
+++ admin/controllers/notifications/templates/index.js
@@ -1,5 +1,18 @@
 angular.module('santedb').controller('NotificationTemplateTableController', ["$scope", "$timeout", function ($scope, $timeout) {
 
+    $scope.delete = async function(id, index) {
+        if(confirm(SanteDB.locale.getString("ui.admin.notifications.template.delete.confirm"))) {
+            try {
+                await SanteDB.resources.notificationTemplate.deleteAsync(id, true);
+                toastr.success(SanteDB.locale.getString("ui.admin.notifications.template.delete.success"));
+                $("#NotificationTemplateTable table").DataTable().draw();
+            }
+            catch(e) {
+                toastr.error(SanteDB.locale.getString("ui.admin.notifications.template.delete.error", { e: e.message }));
+            }
+        }
+    }
+
     async function initializeView() { }
 
     initializeView();
