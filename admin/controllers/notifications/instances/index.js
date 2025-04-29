36601f8cb5b684a6a7a9bbd1820f21f9e51500f1
Ryan Hull <hullr@HHSC.ca>
2025-04-29 12:36:57 -0400

    JIMS-702 Added strings, table columns, and item actions for NotificationInstance table's 'Show Deleted' state

--- admin/controllers/notifications/instances/index.js
+++ admin/controllers/notifications/instances/index.js
@@ -1,5 +1,20 @@
 angular.module('santedb').controller('NotificationInstanceIndexController', ["$scope", "$rootScope", "$state", "$templateCache", function ($scope, $rootScope, $state, $templateCache) {
-    async function initializeView() { }
+    async function initializeView() {
+        
+        $scope.delete = async function(id, index) {
+            if(confirm(SanteDB.locale.getString("ui.admin.notifications.instances.delete.confirm"))) {
+                try {
+                    await SanteDB.resources.notificationInstance.deleteAsync(id, true);
+                    toastr.success(SanteDB.locale.getString("ui.admin.notifications.instances.delete.success"));
+                    $("#NotificationInstanceTable table").DataTable().ajax.reload();
+                }
+                catch(e) {
+                    toastr.error(SanteDB.locale.getString("ui.admin.notifications.instances.delete.error", { e: e.message }));
+                }
+            }
+        }
+
+    }
 
     initializeView();
 }]);
