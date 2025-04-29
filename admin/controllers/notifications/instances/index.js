62c33ea79a08c39743e583c6a34f4a692ee84405
Ryan Hull <hullr@HHSC.ca>
2025-04-29 14:01:58 -0400

    Added render functions for the Created, Deleted, and Last Update properties of the NotificationInstance table

--- admin/controllers/notifications/instances/index.js
+++ admin/controllers/notifications/instances/index.js
@@ -1,6 +1,28 @@
 angular.module('santedb').controller('NotificationInstanceIndexController', ["$scope", "$rootScope", "$state", "$templateCache", function ($scope, $rootScope, $state, $templateCache) {
     async function initializeView() {
         
+        $scope.renderCreationTime = function(r) {
+            if (r.createdBy != null && r.creationTime != null)
+                return `<provenance provenance-id="'${r.createdBy}'" sessionfn="$parent.sessionFunction" provenance-time="'${r.creationTime}'"></provenance>`;
+            return "";
+        }
+
+        $scope.renderObsoletionTime = function(r) {
+            if (r.obsoletedBy != null && r.obsoletionTime != null)
+                return `<provenance provenance-id="'${r.obsoletedBy}'" sessionfn="$parent.sessionFunction" provenance-time="'${r.obsoletionTime}'"></provenance>`;
+            return "";
+        }
+
+        $scope.renderUpdatedTime = function(r) {
+            if (r.obsoletedBy != null)
+                return `<provenance provenance-id="'${r.obsoletedBy}'" sessionfn="$parent.sessionFunction" provenance-time="'${r.obsoletionTime}'"></provenance>`;
+            else if (r.updatedBy != null)
+                return `<provenance provenance-id="'${r.updatedBy}'" sessionfn="$parent.sessionFunction" provenance-time="'${r.updatedTime}'"></provenance>`;
+            else if (r.createdBy != null)
+                return `<provenance provenance-id="'${r.createdBy}'" sessionfn="$parent.sessionFunction" provenance-time="'${r.creationTime}'"></provenance>`;
+            return "";
+        }
+
         $scope.delete = async function(id, index) {
             if(confirm(SanteDB.locale.getString("ui.admin.notifications.instances.delete.confirm"))) {
                 try {
