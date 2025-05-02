f4eb478194cebdb096d40bacfc386a77a6d9ec14
Ryan Hull <hullr@HHSC.ca>
2025-05-02 11:41:38 -0400

    JIMS-700 Updated Edit item action for NotificationInstance entries and added scaffolding for Edit Notification form

---
+++ admin/controllers/notifications/instances/edit.js
@@ -0,0 +1,3 @@
+angular.module('santedb').controller('NotificationsInstanceEditController', ["$scope", "$rootScope", "$state", "$stateParams", "$timeout", function ($scope, $rootScope, $state, $stateParams, $timeout) {
+
+}]);
