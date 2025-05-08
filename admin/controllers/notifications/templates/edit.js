e8bb2006546457815f7ad4525cc5401ca2445cf5
Daria Ostroushko <ostroushko@HHSC.ca>
2025-05-08 18:09:31 -0400

    JIMS-694 Add functionality to differentiate between saving a draft and publishing a template

--- admin/controllers/notifications/templates/edit.js
+++ admin/controllers/notifications/templates/edit.js
@@ -27,36 +27,43 @@ angular.module('santedb').controller('NotificationsTemplateEditController', ["$s
     }
 
     // Save notification template
-    async function saveNotificationTemplate(createNotificationTemplateForm) {
+    async function saveNotificationTemplate(createNotificationTemplateForm, event) {
         if (createNotificationTemplateForm.$invalid) return;
 
-        console.log(createNotificationTemplateForm)
+        // determine whether the template was drafted or published
+        if (event == "publishTemplateButton") {
+            $scope.notificationTemplate.status = "1FAFD226-CAD4-47F2-87E6-7C8F3E9E3B6F"
+        } else if (event == "saveDraftTemplateButton") {
+            $scope.notificationTemplate.status = "0AC6638B-4E72-466A-A20F-1ADB3B8F2139"
+        }
 
-        // try {
-        //     SanteDB.display.buttonWait("#saveNotificationTemplateButton", true);
-        //     // Update
-        //     var notificationTemplate = null;
-        //     if ($stateParams.id) {
-        //         notificationTemplate = await SanteDB.resources.notificationTemplate.updateAsync($stateParams.id, $scope.notificationTemplate);
-        //     }
-        //     else {
-        //         notificationTemplate = await SanteDB.resources.notificationTemplate.insertAsync($scope.notificationTemplate);
-        //     }
-        //     toastr.success(SanteDB.locale.getString("ui.admin.notificationTemplate.save.success"));
+        try {
+            SanteDB.display.buttonWait("#publishTemplateButton", true);
+            SanteDB.display.buttonWait("#saveDraftTemplateButton", true);
+            // Update
+            var notificationTemplate = null;
+            if ($stateParams.id) {
+                notificationTemplate = await SanteDB.resources.notificationTemplate.updateAsync($stateParams.id, $scope.notificationTemplate);
+            }
+            else {
+                notificationTemplate = await SanteDB.resources.notificationTemplate.insertAsync($scope.notificationTemplate);
+            }
+            toastr.success(SanteDB.locale.getString("ui.admin.notificationTemplate.save.success"));
 
-        //     if (!$stateParams.id) {
-        //         $state.go("santedb-admin.notifications.templates.index");
-        //     }
-        //     else {
-        //         $timeout(() => $scope.notificationTemplate = notificationTemplate);
-        //     }
-        // }
-        // catch (e) {
-        //     $rootScope.errorHandler(e);
-        // }
-        // finally {
-        //     SanteDB.display.buttonWait("#saveNotificationTemplateButton", false);
-        // }
+            if (!$stateParams.id) {
+                $state.go("santedb-admin.notifications.templates.index");
+            }
+            else {
+                $timeout(() => $scope.notificationTemplate = notificationTemplate);
+            }
+        }
+        catch (e) {
+            $rootScope.errorHandler(e);
+        }
+        finally {
+            SanteDB.display.buttonWait("#publishTemplateButton", false);
+            SanteDB.display.buttonWait("#saveDraftTemplateButton", false);
+        }
     }
 
     $scope.saveNotificationTemplate = saveNotificationTemplate
