5644dc06939a1a64751a7a60cb3a4b28d1490d36
Daria Ostroushko <ostroushko@HHSC.ca>
2025-05-08 17:47:05 -0400

    JIMS-717 JIMS-718 Added functionality to retrieve and populate template form with new or existing object

--- admin/controllers/notifications/templates/edit.js
+++ admin/controllers/notifications/templates/edit.js
@@ -1,46 +1,68 @@
 angular.module('santedb').controller('NotificationsTemplateEditController', ["$scope", "$rootScope", "$state", "$stateParams", "$timeout", function ($scope, $rootScope, $state, $stateParams, $timeout) {
 
-    function initializeView() {
-        $scope.formData = {
-            parameters: [{}],
-            templates: [{}]
+    async function initializeView(id) {
+        if (id !== undefined) {
+            try {
+                var notificationTemplate = await Promise.all([
+                    await SanteDB.resources.notificationTemplate.getAsync(id, null, null, true),
+                ])
+                console.log(notificationTemplate[0])
+                $scope.notificationTemplate = notificationTemplate[0]
+            }
+            catch (e) {
+                $rootScope.errorHandler(e);
+            }
         }
+        else {
+            $scope.notificationTemplate = new notificationTemplate({
+                id: SanteDB.application.newGuid(),
+                parameters: [{}],
+                contents: [{}]
+            })
+        }
+
         $scope.newParameter = {}
         $scope.newTemplate = {}
+
     }
 
     // Save notification template
     async function saveNotificationTemplate(createNotificationTemplateForm) {
         if (createNotificationTemplateForm.$invalid) return;
 
-        try {
-            SanteDB.display.buttonWait("#saveNotificationTemplateButton", true);
-            // Update
-            var notificationTemplate = null;
-            if ($stateParams.id) {
-                notificationTemplate = await SanteDB.resources.notificationTemplate.updateAsync($stateParams.id, $scope.notificationTemplate);
-            }
-            else {
-                notificationTemplate = await SanteDB.resources.notificationTemplate.insertAsync($scope.notificationTemplate);
-            }
-            toastr.success(SanteDB.locale.getString("ui.admin.notificationTemplate.save.success"));
+        console.log(createNotificationTemplateForm)
 
-            if (!$stateParams.id) {
-                $state.go("santedb-admin.notifications.templates.index");
-            }
-            else {
-                $timeout(() => $scope.notificationTemplate = notificationTemplate);
-            }
-        }
-        catch (e) {
-            $rootScope.errorHandler(e);
-        }
-        finally {
-            SanteDB.display.buttonWait("#saveNotificationTemplateButton", false);
-        }
+        // try {
+        //     SanteDB.display.buttonWait("#saveNotificationTemplateButton", true);
+        //     // Update
+        //     var notificationTemplate = null;
+        //     if ($stateParams.id) {
+        //         notificationTemplate = await SanteDB.resources.notificationTemplate.updateAsync($stateParams.id, $scope.notificationTemplate);
+        //     }
+        //     else {
+        //         notificationTemplate = await SanteDB.resources.notificationTemplate.insertAsync($scope.notificationTemplate);
+        //     }
+        //     toastr.success(SanteDB.locale.getString("ui.admin.notificationTemplate.save.success"));
+
+        //     if (!$stateParams.id) {
+        //         $state.go("santedb-admin.notifications.templates.index");
+        //     }
+        //     else {
+        //         $timeout(() => $scope.notificationTemplate = notificationTemplate);
+        //     }
+        // }
+        // catch (e) {
+        //     $rootScope.errorHandler(e);
+        // }
+        // finally {
+        //     SanteDB.display.buttonWait("#saveNotificationTemplateButton", false);
+        // }
     }
 
     $scope.saveNotificationTemplate = saveNotificationTemplate
 
-    initializeView()
+    if ($stateParams.id) {
+        $scope.isLoading = true;
+    }
+    initializeView($stateParams.id)
 }]);
