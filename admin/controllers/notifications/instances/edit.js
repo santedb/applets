3d1c15f1a06d93e378d7855b20ab6a993472bd78
Jordan Webber <webberjo@hhsc.ca>
2025-05-09 16:46:51 -0400

    JIMS-705 Edit Notification Instance populates the form with Notification Instance values

--- admin/controllers/notifications/instances/edit.js
+++ admin/controllers/notifications/instances/edit.js
@@ -1,3 +1,49 @@
 angular.module('santedb').controller('NotificationsInstanceEditController', ["$scope", "$rootScope", "$state", "$stateParams", "$timeout", function ($scope, $rootScope, $state, $stateParams, $timeout) {
+    async function initializeView(id) {
+        if (id !== undefined) {
+            try {
+                const notificationInstance = await SanteDB.resources.notificationInstance.getAsync(id, null, null, true);
+                $scope.notificationInstance = notificationInstance;
+            } catch (e) {
+                $rootScope.errorHandler(e);
+            }
+        } else {
+            $scope.notificationInstance = new notificationInstance({
+                id: SanteDB.application.newGuid(),
+                parameters: [{}],
+            }) 
+        }
+    }
+    
+    $scope.saveNotificationInstance = async function(notificationInstanceForm, event) {
+        console.log($scope.notificationInstance);
+        if (notificationInstanceForm.$invalid) return;
 
+
+        try {
+            SanteDB.display.buttonWait("#saveNotificationInstanceButton", true);
+            SanteDB.display.buttonWait("#cancelNotificationInstanceButton", true);
+
+            var notificationInstance = null;
+
+            notificationInstance = await SanteDB.resources.notificationInstance.updateAsync($stateParams.id, $scope.notificationInstance);
+            
+            toastr.success(SanteDB.locale.getString("ui.admin.notification.instance.save.success"));
+            
+            $state.go("santedb-admin.notifications.Instances.index");
+        }
+        catch (e) {
+            $rootScope.errorHandler(e);
+        }
+        finally {
+            SanteDB.display.buttonWait("#saveNotificationInstanceButton", false);
+            SanteDB.display.buttonWait("#cancelNotificationInstanceButton", false);
+        }
+    }
+
+    if($stateParams.id) {
+        $scope.isLoading = true;
+    }
+
+    initializeView($stateParams.id)
 }]);
