484bed40fcf12f642b0ce019f605667f8fca335d
Jordan Webber <webberjo@hhsc.ca>
2025-05-14 14:49:27 -0400

    JIMS-688 Added CDSS editor to the new and edit Notification Instance forms

--- admin/controllers/notifications/instances/create.js
+++ admin/controllers/notifications/instances/create.js
@@ -1,18 +1,29 @@
+/// <reference path="../../../../core/js/santedb.js"/>
+/// <reference path="../../cdss/codeEditor.js"/>
+
 angular.module('santedb').controller('NewNotificationController', ["$scope", "$rootScope", "$state", "$stateParams", "$timeout", function ($scope, $rootScope, $state, $stateParams, $timeout) {
 
     async function initializeView() {
         $scope.notificationInstance = {
             $type: 'NotificationInstance',
             state: 'AC843892-F7E0-47B6-8F84-11C14E7E96C6',
+            filter: '',
             template: null,
             parameters: [],
         }
+
+        const libraryDefinition = await SanteDB.resources.cdssLibraryDefinition.getAsync(null, null, null, true, { "oid": "1.3.6.1.4.1.52820.5.1.5.9.1" });
+        $timeout(() => {
+            $scope.cdssLibrary = libraryDefinition.resource[0].library;
+        });
+        
+        $scope.cdssEditor = new CdssAceEditor("cdssEditor", $scope.notificationInstance.filter, $scope.cdssLibrary.id, $scope.cdssLibrary.uuid);
     }
     
     $scope.saveNotificationInstance = async function(notificationInstanceForm, event) {
         if (notificationInstanceForm.$invalid) return;
 
-        console.log($scope.notificationInstance)
+        $scope.notificationInstance.filter = document.getElementById("cdssEditor").innerText;
 
         try {
             SanteDB.display.buttonWait("#saveNotificationInstanceButton", true);
--- admin/controllers/notifications/instances/edit.js
+++ admin/controllers/notifications/instances/edit.js
@@ -1,28 +1,43 @@
-angular.module('santedb').controller('NotificationsInstanceEditController', ["$scope", "$rootScope", "$state", "$stateParams", "$timeout", function ($scope, $rootScope, $state, $stateParams, $timeout) {
+/// <reference path="../../../../core/js/santedb.js"/>
+/// <reference path="../../cdss/codeEditor.js"/>
+
+angular.module('santedb').controller('NotificationsInstanceEditController', ["$scope", "$rootScope", "$state", "$stateParams", "$timeout", "$interval", function ($scope, $rootScope, $state, $stateParams, $timeout, $interval) {
+    
     async function initializeView(id) {
         if (id !== undefined) {
             try {
-                const notificationInstance = await SanteDB.resources.notificationInstance.getAsync(id, null, null, true, null);
-                
+                const notificationInstance = await SanteDB.resources.notificationInstance.getAsync(id, "full", null, true, null);
+                console.log(notificationInstance)
                 setTimeout(() => {
                     $scope.notificationInstance = notificationInstance
                 })
             } catch (e) {
                 $rootScope.errorHandler(e);
+            } finally {
+                $scope.isLoading = false;
             }
         } else {
             $scope.notificationInstance = {
                 $type: 'NotificationInstance',
                 state: 'AC843892-F7E0-47B6-8F84-11C14E7E96C6',
+                filter: '',
                 template: null,
                 parameters: [],
             }
         }
+
+        const libraryDefinition = await SanteDB.resources.cdssLibraryDefinition.getAsync(null, null, null, true, { "oid": "1.3.6.1.4.1.52820.5.1.5.9.1" });
+        $timeout(() => {
+            $scope.cdssLibrary = libraryDefinition.resource[0].library;
+        });
+        
+        $scope.cdssEditor = new CdssAceEditor("cdssEditor", $scope.notificationInstance.filter, $scope.cdssLibrary.id, $scope.cdssLibrary.uuid);
     }
     
     $scope.saveNotificationInstance = async function(notificationInstanceForm, event) {
         if (notificationInstanceForm.$invalid) return;
 
+        $scope.notificationInstance.filter = document.getElementById("cdssEditor").innerText;
 
         try {
             SanteDB.display.buttonWait("#saveNotificationInstanceButton", true);
@@ -47,11 +62,19 @@ angular.module('santedb').controller('NotificationsInstanceEditController', ["$s
         $scope.isLoading = true;
     }
 
-    initializeView($stateParams.id)
+    $scope.cdssLibrary = {}
+
+
+    initializeView($stateParams.id);
+
+    $scope.goToNewTemplate = async function() {
+        $state.go("santedb-admin.notifications.templates.create")
+    }
 
     $scope.$watch("notificationInstance.template", async function(n, o) {
         if(n != o && n) {
-            const template = await SanteDB.resources.notificationTemplate.getAsync($scope.notificationInstance.template, null, null, true)
+            console.log('here')
+            const template = await SanteDB.resources.notificationTemplate.getAsync($scope.notificationInstance.id, null, null, true)
 
             $timeout(() => {
                 $scope.notificationInstance.parameters = template.parameters           
