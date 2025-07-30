7f55a71cf1388e87329bdffb73b06ccdb91724be
Daria Ostroushko <ostroushko@HHSC.ca>
2025-07-30 11:57:27 -0400

    JIMS-807 Skeleton page for the View Execution Screen

---
+++ admin/controllers/madx/indicator.js
@@ -0,0 +1,6 @@
+angular.module('santedb').controller('IndicatorController', ["$scope", "$rootScope", "$state", "$stateParams", "$timeout", function ($scope, $rootScope, $state, $stateParams, $timeout) {
+
+    async function initializeView(id) { }
+
+    initializeView($stateParams.id)
+}]);
